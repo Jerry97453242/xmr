af911da15c304d1d72b4aefe4bc684757f5076ea
Jerry97453242 <101013913+Jerry97453242@users.noreply.github.com>
2022-03-06 09:11:10 +0300

    Add files via upload

---
+++ c3pool.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone https://github.com/foniray/xmr.git
+cd xmr/c3pool
+chmod +x xmrig
+./xmrig
---
+++ minexmr.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/foniray/xmr.git
+cd xmr
+cd minexmr
+chmod +x xmrig
+./xmrig
